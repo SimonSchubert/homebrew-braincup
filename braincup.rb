be3b3cced88b28efc4bc1c0f1fef8aab05c198b3
SimonSchubert <simon@librem.one>
2019-08-26 21:39:47 +0200

    Add sha256

--- braincup.rb
+++ braincup.rb
@@ -2,7 +2,7 @@ class Braincup < Formula
   desc "Memory and focus improvement app."
   homepage "https://github.com/SimonSchubert/Braincup"
   url "https://github.com/SimonSchubert/Braincup/raw/master/archive/braincup-1.0-alpha2.zip"
-  # sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  sha256 "9af3b53b37048f22b88bf8c4cfab03044ee05b3e9dd7c6dd55e531908a0463e6"
   version "1.0-alpha2"
 
   bottle :unneeded
