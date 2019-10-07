5b68084c401fbcb9e5c023ef066ac7c0a40c992c
Simon Schubert <sschubert89@gmail.com>
2019-10-08 00:53:07 +0200

    Update to 1.0-alpha3

--- braincup.rb
+++ braincup.rb
@@ -1,9 +1,9 @@
 class Braincup < Formula
   desc "Memory and focus improvement app."
   homepage "https://github.com/SimonSchubert/Braincup"
-  url "https://github.com/SimonSchubert/Braincup/raw/master/archive/braincup-1.0-alpha2.zip"
-  sha256 "9af3b53b37048f22b88bf8c4cfab03044ee05b3e9dd7c6dd55e531908a0463e6"
-  version "1.0-alpha2"
+  url "https://github.com/SimonSchubert/Braincup/raw/master/archive/braincup-1.0-alpha3.zip"
+  sha256 "211aaf28519067960d179de5c57f6d3acc48088962f95159b22dd23199a764fc"
+  version "1.0-alpha3"
 
   bottle :unneeded
 
