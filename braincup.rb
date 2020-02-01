a7f996f409e8aed2a91611c44afca84aa5e2fc1f
Simon Schubert <sschubert89@gmail.com>
2020-02-01 17:11:05 +0100

    Update to 1.4.2

--- braincup.rb
+++ braincup.rb
@@ -1,9 +1,9 @@
 class Braincup < Formula
   desc "Train your math skills, memory and focus."
   homepage "https://github.com/SimonSchubert/Braincup"
-  url "https://github.com/SimonSchubert/Braincup/raw/master/archive/braincup-1.4.1.zip"
-  sha256 "aa5054484fe5cf6eb678e959b6bf3b941a4986f24f5a94c0708c2be4d8947924"
-  version "1.4.1"
+  url "https://github.com/SimonSchubert/Braincup/raw/master/archive/braincup-1.4.2.zip"
+  sha256 "cde64d7c802d1d657d2e3079f36b68017f12fd9488953b2fb62d54c768153314"
+  version "1.4.2"
 
   bottle :unneeded
 
